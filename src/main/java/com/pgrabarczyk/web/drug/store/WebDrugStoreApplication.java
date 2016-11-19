3fc4a64352a829178095389cb577386a2b86be9f
Patryk Grabarczyk <grabarczyk.patryk@gmail.com>
2016-11-19 13:56:34 +0100

    Initial commit

---
+++ src/main/java/com/pgrabarczyk/web/drug/store/WebDrugStoreApplication.java
@@ -0,0 +1,12 @@
+package com.pgrabarczyk.web.drug.store;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebDrugStoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebDrugStoreApplication.class, args);
+	}
+}
