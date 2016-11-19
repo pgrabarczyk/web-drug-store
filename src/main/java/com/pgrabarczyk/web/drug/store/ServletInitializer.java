9fd07ec792d03822f0097990db1a7afec52109a5
Patryk Grabarczyk <grabarczyk.patryk@gmail.com>
2016-11-19 13:56:34 +0100

    Initial commit

---
+++ src/main/java/com/pgrabarczyk/web/drug/store/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.pgrabarczyk.web.drug.store;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(WebDrugStoreApplication.class);
+	}
+
+}
---
+++ src/test/java/com/pgrabarczyk/pharmacy/PharmacyApplicationTests.java
@@ -0,0 +1,16 @@
+package com.pgrabarczyk.pharmacy;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class PharmacyApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
