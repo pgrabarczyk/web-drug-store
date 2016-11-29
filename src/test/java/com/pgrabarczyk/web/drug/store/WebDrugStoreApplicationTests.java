8f3085f88c61443969ad8002d4b426ea06e91381
pgrabarczyk <pgrabarczyk@laptop-gda1.pgs-soft.com>
2016-11-29 20:35:41 +0100

    Test rename

--- src/test/java/com/pgrabarczyk/web/drug/store/WebDrugStoreApplicationTests.java
+++ src/test/java/com/pgrabarczyk/web/drug/store/WebDrugStoreApplicationTests.java
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class PharmacyApplicationTests {
+public class WebDrugStoreApplicationTests {
 
 	@Test
 	public void contextLoads() {
